6145c8f4259e2b3516f5d4f248fd0746656d2ba1
odhissm <74164097+odhissm@users.noreply.github.com>
2023-08-05 16:36:01 -0400

    added truffle config file
    added truffle config file

---
+++ tests/truffle-config.js
@@ -0,0 +1,11 @@
+module.export = {
+    networks: {
+        // Define your network configurations here (e.g., development, ropsten, mainnet, etc.)
+    },
+    compilers: {
+        solc: {
+            version: "0.8.0", // Use the desired Solidity compiler version
+
+        },
+    },
+};

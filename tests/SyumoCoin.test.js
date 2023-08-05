e76712d5f033a9ed616832a6666ba43f4b17507e
odhissm <74164097+odhissm@users.noreply.github.com>
2023-08-05 14:44:47 -0400

    Updated SyumoCoin.test.js
    Updated SyumoCoin.test.js
    
    add a test case

--- tests/SyumoCoin.test.js
+++ tests/SyumoCoin.test.js
@@ -25,4 +25,23 @@ contract("SyumoCoin", (accounts) => {
     assert.equal(senderBalance.toNumber(), 100000 * 10**18 - amount, "Incorrect sender balance");
     assert.equal(receiverBalance.toNumber(), amount, "Incorrect receiver balance");
   });
+
+  it("should not transfer tokens if trading is not enabled", async () => {
+    const sender = accounts[0];
+    const receiver = accounts[1];
+    const amount = 1000;
+
+    try {
+      await syumoCoinInstance.transfer(receiver, amount, { from: sender });
+      assert.fail("Transfer should not be allowed before trading is enabled.");
+    } catch (error) {
+      assert(error.message.includes("Trading is not yet enabled."), "Expected error message not received.");
+    }
+
+    const senderBalance = await syumoCoinInstance.balanceOf(sender);
+    const receiverBalance = await syumoCoinInstance.balanceOf(receiver);
+
+    assert.equal(senderBalance.toNumber(), 100000 * 10**18, "Sender balance should not change");
+    assert.equal(receiverBalance.toNumber(), 0, "Receiver balance should remain 0");
+  });
 });
